cdc990a323f61b527fb023004824cad402ded8d6
Sho Fujiwara <shofujiwara505@gmail.com>
2023-02-21 13:37:54 +0900

    WOW AMAZING WEBSITE BUILT ONLY IN 1 DAY

---
+++ react101/src/components/FunctionalComponent.jsx
@@ -0,0 +1,7 @@
+export default class Welcome {
+  render() {
+    return console.log(
+      "Welcome to the jungle from the exported FunctionalComponent.jsc in ./components/"
+    );
+  }
+}
---
+++ react101/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Welcome from "./components/FunctionalComponent.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+console.log("welcome to the jungle from index.js");
+// super important cause this calls app which has all the componenets, ad imports here if you want to make more componenets or in App.js
